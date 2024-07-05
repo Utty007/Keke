be577fe8324e18baeed2ad6e72f52c693b9baaec
Kayode Uthman <bhamisheyekaydee@gmail.com>
2024-07-05 11:37:56 -0700

    SEO update

--- app/components/DownloadApp.tsx
+++ app/components/DownloadApp.tsx
@@ -8,7 +8,7 @@ function DownloadApp() {
       <div id='app' className='bg-white flex-col-reverse flex md:flex-row gap-6 p-8 items-center'>
         <Image src={appInterface} alt='App Interface' /> 
         <div className='max-w-[600px]'>
-            <h1 className='text-5xl font-bold text-[#f4c617] leading-normal'>Anticipate Our App</h1>
+            <h3 className='text-5xl font-bold text-[#f4c617] leading-normal'>Anticipate Our App</h3>
             <p>The Keke app is a smart bike sharing application designed for institutions, offering a seamless and user-friendly experience. Join the green movement with Keke and enjoy a healthier, eco-friendly commuting option with our secure and reliable bicycle system. <br /> <br /> <span className='font-bold text-xl'>Coming Soon To Playstore And Appstore!</span></p>
             <div className='hidden'>
                 <button className='flex flex-row bg-[#f4c617] text-black px-3 py-1 items-center gap-1'>
